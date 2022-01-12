a5cf6c826e7c5c199278b520091e2e5f49d2c14c
vicbolt <vicbolt.madrid@gmail.com>
2022-01-12 01:27:20 +0100

    no multer

--- components/SfSignUpForm.vue
+++ components/SfSignUpForm.vue
@@ -14,8 +14,12 @@
             <v-text-field v-model="password" type="password" placeholder="Contraseña" outlined class="mb-n4"/>
             <v-text-field v-model="password2" type="password" placeholder="Repita la contraseña" outlined class="mb-n2"/>
             <v-row>
-                <v-col cols="2">
-                    <v-file-input v-model="avatar"  height="80px" outlined prepend-icon="" placeholder="AVATAR" filled> </v-file-input>
+                <v-col cols="8" id="center">
+                    <div class="file-container">
+                        <h5 v-if="!url" id="h5">SUBIR AVATAR</h5>
+                        <img id="img" v-if="url" :src="url" alt="Image not found" @click="picAgain">
+                        <input v-if="!url" type="file" @change="showImg" ref="file" id="imgBtn" class="mt-3" style="background: red; border: 2px solid white; overflow: hidden"/>
+                    </div>                   
                 </v-col>
                 <v-col cols="9"></v-col>
             </v-row>
@@ -45,28 +49,33 @@ export default ({
             password2: "",
             avatar: undefined,
             error: "",
-            msg: ""
+            msg: "",
+            url: "",
         }
     },
 
     methods: {
         async submit(){
 
-                const formData = new FormData()
-                    formData.enctype = "multipart/form-data",
-                    formData.append('email', this.email)
-                    formData.append('username', this.username)
-                    formData.append('password', this.password)
-                    formData.append('password2', this.password2)
-                    formData.append('avatar', this.avatar)
-
                 try{
+
+                const body = JSON.stringify({
+                    title:  this.title,
+                    director: this.director,
+                    description: this.description,
+                    score: this.score,
+                    userId: userId,
+                    image: this.url
+                })
 
                 const config = require('../config')
 
                 const res = await fetch(config.hostname + 'api/user/signUp', {
                     method: 'post',
-                    body: formData
+                    headers: {
+                        "Content-Type" : "application/json"
+                    },
+                    body
                 })
 
                 const data = await res.json()
@@ -81,13 +90,29 @@ export default ({
                         
                     }
 
-                
-                
-
-
             } catch(error){
                 return res.status(500).json(error)
             }
+        },
+
+            showImg(){
+
+            const file = this.$refs.file.files[0]
+            const reader = new FileReader()
+            reader.onloadend = () => {
+                console.log(reader.result)
+                this.url = reader.result
+                console.log("ESTO ES THIS.URL" ,this.url)
+            }
+
+            if(file){
+                reader.readAsDataURL(file)
+            }
+            
+        },
+
+        picAgain(){
+            this.url = ""
         },
 
     }
@@ -123,4 +148,42 @@ h1{
     margin-bottom: 7px;
     border: 2px solid rgb(229,9,20);
 }
+
+.file-container{
+    background-color: rgb(255, 255, 255);
+    height: 107px;
+    width: 107px;
+    border: 2px solid white;
+    border-radius: 100%;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    position: relative;
+}
+
+#img{
+    border-radius: 100%;
+    height: 100px;
+    width: 100px;
+    cursor: pointer;
+}
+
+#imgBtn{
+    position: absolute;
+    opacity: 0;
+    width: 100px;
+    height: 100px;
+    cursor: pointer;
+    z-index: 2;
+}
+
+#h5{
+    position: center absolute;
+    display: flex;
+    color: rgb(0, 0, 0);
+    z-index: 1;
+    margin-left: 4px;
+    cursor: pointer;
+}
+
 </style>
